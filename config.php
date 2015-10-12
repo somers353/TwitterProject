43bce31c83a7a40c4ea1d97546c3dfc3651fb4e4
somers353 <legitimate_strategy@hotmail.co.uk>
2015-10-12 11:24:41 +0100

    Edit config.php
    Removed passwords and keys

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$CONSUMER_KEY="f4i2juufJAQCMGI3mTIyI4J7d";
-$CONSUMER_SECRET="nYJ8grXv1IRCtj1KH7GlsMxjHt5FcINKUKOqwGVNv4DkP7kQZ7";
+$CONSUMER_KEY="";
+$CONSUMER_SECRET="";
 $OAUTH_CALLBACK="http://www.keithandnikkiswedding.com/Twitter/oauth.php";
 $host="mysql1.host.ie";
-$username="soke96_wpdb";
-$pass="Rev0luti0n";
+$username="";
+$pass="";
 ?>
