b260223031748efeeacb898c207a9f8031604d5f
somers353 <legitimate_strategy@hotmail.co.uk>
2015-10-12 11:16:31 +0100

    Commit complete version

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<div align="center">
+<h2>Login with Twitter</h2>
+
+Click on the image to log in to Twitter. <br/>
+<a href="login.php"><img src="LoginTwitter.png"/></a>
+</div>
+</body>
+
+</html>
